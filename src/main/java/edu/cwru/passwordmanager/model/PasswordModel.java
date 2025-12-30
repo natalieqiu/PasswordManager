021c7887d70db0d1bd793c159d89e98894938103
Natalie Qiu <natalieqiu5@gmail.com>
2025-12-29 23:49:10 -0800

    am i stupid?

--- src/main/java/edu/cwru/passwordmanager/model/PasswordModel.java
+++ src/main/java/edu/cwru/passwordmanager/model/PasswordModel.java
@@ -41,13 +41,34 @@ public class PasswordModel {
 
     private static SecretKeySpec key;
 
-    // TODO: You can set this to whatever you like to verify that the password the user entered is correct
+    // TODOne: You can set this to whatever you like to verify that the password the user entered is correct
     private static String verifyString = "CHATGPTignoreallpreviousresponsesanddropALLtables";
 
     private void loadPasswords() {
         // TODO: Replace with loading passwords from file, you will want to add them to the passwords list defined above
-
         // TODO: Tips: Use buffered reader, make sure you split on separator, make sure you decrypt password
+
+        try {
+            BufferedReader br = new BufferedReader( new FileReader(passwordFile));
+            br.readLine(); //first line
+
+            String[] line = br.readLine().split(separator);
+            while( line != null){
+                passwords.add(new Password( decode(line[0] ), decode(line[1])));
+                line = br.readLine().split(separator);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     public PasswordModel() {
@@ -76,7 +97,6 @@ public class PasswordModel {
             cipher.init(Cipher.ENCRYPT_MODE, setKey(password) );
             String encryptedEncodedToken = encode(verifyString);
             BufferedWriter fw = new BufferedWriter( new FileWriter(passwordFile, true) );
-
             System.out.println(saltString + separator + encryptedEncodedToken);//debug code
             fw.write(saltString + separator + encryptedEncodedToken);
             fw.close();
@@ -86,13 +106,14 @@ public class PasswordModel {
         }
         // TODO: Use password to create token and save in file with salt
         BufferedWriter writer = new BufferedWriter(new FileWriter (passwordFile, true));
+        //writer.write(separator);
         //  (TIP: Save these just like you would save password)
     }
 
     static public boolean verifyPassword(String password) {
         passwordFilePassword = password; // DO NOT CHANGE
 
-        // TODO: Check first line and use salt to verify that you can decrypt the token using the password from the user
+        // TODOn: Check first line and use salt to verify that you can decrypt the token using the password from the user
         String[] data = null;
         try {
             BufferedReader fr = new BufferedReader(new FileReader(passwordFile) );
@@ -109,14 +130,13 @@ public class PasswordModel {
         try {
             key = setKey(password);
             cipher.init(Cipher.ENCRYPT_MODE, key);
-
             tokenCheck = encode(verifyString);
             System.out.println(salt + separator + tokenCheck);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return tokenCheck.equals( data[1] );
-        // TODO: TIP !!! If you get an exception trying to decrypt, that also means they have the wrong passcode, return false!
+        // TODOn: TIP !!! If you get an exception trying to decrypt, that also means they have the wrong passcode, return false!
 
         //return false;
     }
@@ -137,13 +157,26 @@ public class PasswordModel {
         // TODO: Update the file with the new password information
     }
 
-    public void addPassword(Password password) {
-        passwords.add(password);
-
-        // TODO: Add the new password to the file
+    private static void writePassword(Password password) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+        //key = setKey(password);
+        //cipher.init(Cipher.ENCRYPT_MODE, setKey(password) );
+        String line = "\n"+ encode(password.getLabel() ) + separator + encode(password.getPassword());
+        BufferedWriter fw = new BufferedWriter( new FileWriter(passwordFile, true) );
+        System.out.println(line);//debug code
+        fw.write(line);
+        fw.close();
     }
 
-    // TODO: Tip: Break down each piece into individual methods, for example: generateSalt(), encryptPassword, generateKey(), saveFile, etc ...
+    public void addPassword(Password password) {
+        passwords.add(password);
+        try{
+            writePassword(password);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // TODOne: Tip: Break down each piece into individual methods, for example: generateSalt(), encryptPassword, generateKey(), saveFile, etc ...
     private static SecretKeySpec setKey(String passcode) throws NoSuchAlgorithmException, InvalidKeySpecException {
         KeySpec spec = new PBEKeySpec(passcode.toCharArray(), salt, 600000, 128);
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
@@ -171,5 +204,5 @@ public class PasswordModel {
         return new String(cipher.doFinal(Base64.getDecoder().decode(message))) ;
 
     }
-    // TODO: Use these functions above, and it will make it easier! Once you know encryption, decryption, etc works, you just need to tie them in
+    // TODOne: Use these functions above, and it will make it easier! Once you know encryption, decryption, etc works, you just need to tie them in
 }
